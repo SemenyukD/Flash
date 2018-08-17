d474daa5df3213f18e2acea85d63f1d7eea2682d
Semenyuk Dmytro <semenyuk.c.s@gmail.com>
2018-08-17 17:07:36 +0300

    Hello

---
+++ Test/src/ReportCard2.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class ReportCard2 {
+
+	int i;
+	
+	public void Scan() {
+		System.out.println("¬вед≥ть ¬ашу оц≥нку");
+		Scanner scn = new Scanner (System.in);
+		i = scn.nextInt();
+	}
+}
+
