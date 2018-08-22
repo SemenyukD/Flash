90452acc1da3dcf6ac6f34c32dfc86ba1788fad7
SemenyukD <42452896+SemenyukD@users.noreply.github.com>
2018-08-22 13:51:01 +0300

    Update ReportCard.java

--- Test/src/ReportCard.java
+++ Test/src/ReportCard.java
@@ -1,89 +1,94 @@
 
 public class ReportCard {
-	//char grade;
-	public static void main (String [] args) {
-		
-		ReportCard2 rc = new ReportCard2 ();
-		
-		rc.Scan();
-		rc.i = convertGrade (rc.i);
+
+	public static void main(String[] args) {
+
+		ReportCard2 rc = new ReportCard2();
+
+		rc.scan();
+		rc.i = convertGrade(rc.i);
 	}
-	/** 
-	 Метод ConvertGrades принимает один цельночисельнный агрумент
-	 - результат теста и результат теста и возвращает символ A, B, C or D в
-	 зависимости от этого аргумента.
+
+	/**
+	 * Метод ConvertGrades принимает один цельночисельнный агрумент - результат
+	 * теста и результат теста и возвращает символ A, B, C or D в зависимости от
+	 * этого аргумента.
 	 */
-public static char convertGrade (int testResults) {
-	
-	char grade = 0; // локальна змінна
-	
-	switch (grade){   
-	
-	case 0:  
-	if (testResults>=90) {
-	grade = 'A';
-	System.out.println("Ваша оцінка " + grade + "\n" + "Чудова робота!");
-	break;
-	}else if (testResults>=80 && testResults<90) {
-	grade = 'B';
-	System.out.println("Ваша оцінка " + grade + "\n" + "Хороша робота!");
-	break;
-	}else if (testResults>=70 && testResults<80) {
-	grade = 'C';
-	System.out.println("Ваша оцінка " + grade + "\n" + "Потрібно підтягнути знання!");
-	break;
+	public static char convertGrade(int testResults) {
+
+		char grade = 0;// локальна змінна
+
+		if (testResults >= 90) {
+			grade = 'A';
+			//System.out.println("Ваша оцінка " + grade + "\n" + "Чудова оцінка!");
+		} else if (testResults >= 80 && testResults < 90) {
+			grade = 'B';
+			//System.out.println("Ваша оцінка " + grade + "\n" + "Хороша робота!");
+		} else if (testResults >= 70 && testResults < 80) {
+			grade = 'C';
+			//System.out.println("Ваша оцінка " + grade + "\n" + "Потрібно підтягнути знання!");
+		} else {
+			grade = '0';
+		}
+
+		if (Character.isLetter(grade))
+			;
+		{
+
+			switch (grade) {
+			case 'A': {
+				System.out.println("Ваша оцінка " + grade + "\n" + "Чудова робота!");
+			}
+				break;
+			case 'B': {
+				System.out.println("Ваша оцінка " + grade + "\n" + "Хороша робота!");
+			}
+				break;
+			case 'C': {
+				System.out.println("Ваша оцінка " + grade + "\n" + "Потрібно підтягнути знання!");
+			}
+				break;
+			}
+			return grade;
+		}
 	}
-	case 1:    
-	break;     
-	}
-	return grade;
 }
-}
-	
-	
-	
-	
-/**	
-	if (testResults>=90) {
-	grade = 'A';
-	//System.out.println("Ваша оцінка " + grade + "\n" + "Чудова оцінка!");
-} else if (testResults>=80 && testResults<90) {
-	grade = 'B';
-	//System.out.println("Ваша оцінка " + grade + "\n" + "Хороша робота!");
-} else if (testResults>=70 && testResults<80) {
-	grade = 'C';
-	//System.out.println("Ваша оцінка " + grade + "\n" + "Потрібно підтягнути знання!"); 
-} else {
-	grade = '0';
-}
-	//if (Character.isLetter(grade));{
-	
-	switch (grade){     
-	case 'A':  {      
-	System.out.println("Ваша оцінка " + grade + "\n" + "Чудова робота!");        
-	break;     
-	}case 'B': {        
-	System.out.println("Ваша оцінка " + grade + "\n" + "Хороша робота!");        
-	}break;     
-	case 'C': {       
-	System.out.println("Ваша оцінка " + grade + "\n" + "Потрібно підтягнути знання!");        
-	}break; 
-}
-	return grade;
-}
-}
+
+
+
+
 
 //public static void main (String [] args) {
-	
-	//ReportCard2 rc = new ReportCard2 ();
-	
-		//char yourGrade = rc.convertGrade (88);
-		//System.out.println("Ваша перша оцінка" + yourGrade);
-		
-		//yourGrade = rc.convertGrade(79);
-		//System.out.println("Ваша друга оцінка " + yourGrade);
-		
-*/
+
+// ReportCard2 rc = new ReportCard2 ();
+
+// char yourGrade = rc.convertGrade (88);
+// System.out.println("Ваша перша оцінка" + yourGrade);
+
+// yourGrade = rc.convertGrade(79);
+// System.out.println("Ваша друга оцінка " + yourGrade);
 
 
 
+//switch (grade) {
+
+//case 0:
+	//if (testResults >= 90) {
+		//grade = 'A';
+		//System.out.println("Ваша оцінка " + grade + "\n" + "Чудова робота!");
+		//break;
+	//} else if (testResults >= 80 && testResults < 90) {
+		//grade = 'B';
+		//System.out.println("Ваша оцінка " + grade + "\n" + "Хороша робота!");
+		//break;
+	//}else if (testResults >= 70 && testResults < 80) {
+		//grade = 'C';
+		//System.out.println("Ваша оцінка " + grade + "\n" + "Потрібно підтягнути знання!");
+		//break;
+	//}
+//case 1:
+	//break;
+//}
+//return grade;
+//}
+//}
